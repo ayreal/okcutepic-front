fa362d5877678b5fea36aceeed9bb8df106a8f3d
Ariel Herman <aeh.herman@gmail.com>
2017-12-01 16:13:57 -0500

    routes up

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react";
 
 import FeaturesContainer from "./FeaturesContainer";
-import AppHeader from "./AppHeader";
 
 class MainContainer extends Component {
   render() {
     return (
       <div>
         <FeaturesContainer />
-        <AppHeader />
       </div>
     );
   }
