1034a8af0dc6a4043bc8a0aa3aa406315d060884
Ariel Herman <aeh.herman@gmail.com>
2017-12-03 11:53:44 -0500

    style header

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { Container, Menu } from "semantic-ui-react";
+import { Container, Menu, Button } from "semantic-ui-react";
 
-const AppHeader = () => (
-  <Menu fixed="top" inverted>
+const AppHeader = props => (
+  <Menu fixed="top" color="purple" inverted>
     <Container>
-      <Menu.Item as="a" header>
+      <Menu.Item as="h1" header>
         OkCutepic
       </Menu.Item>
-      <Menu.Item as="a">Home</Menu.Item>
+      <Menu.Item as="h3">Welcome, {props.user.name}!</Menu.Item>
+      <Menu.Menu position="right">
+        <Menu.Item>
+          <Button default>Logout</Button>
+        </Menu.Item>
+      </Menu.Menu>
     </Container>
   </Menu>
 );
