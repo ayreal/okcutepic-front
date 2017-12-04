8612fc4d1a8241f291e3a71c7a3a68cdaab3be56
Ariel Herman <aeh.herman@gmail.com>
2017-12-04 08:38:34 -0500

    merge with matt

--- src/components/Adapter.js
+++ src/components/Adapter.js
@@ -1,10 +1,17 @@
 const ROUTE = "https://okcutepic-back.herokuapp.com/api/v1";
+const TEST_ROUTE = "http://localhost:3001/api/v1";
+const headers = {
+  Accepts: "application/json, text/plain",
+  "Content-Type": "application/json"
+};
 
-// const headers = {
-//   Accepts: "application/json, text/plain",
-//
-//   "Content-Type": "application/json"
-// };
+export function login(data) {
+  return fetch(`${TEST_ROUTE}/auth/`, {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(data)
+  }).then(res => res.json);
+}
 
 export function fetchUsers() {
   return fetch(`${ROUTE}/users`).then(res => res.json());
